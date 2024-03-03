522e179dc7824aa80faf20e0de6dad96a8328708
togrul98 <togrulmamedof@mail.ru>
2024-03-04 02:14:05 +0400

    Tapşırıq1

---
+++ index.js
@@ -0,0 +1,12 @@
+var age = prompt('Yasiniz')
+
+var ageInt = parseInt(age)
+
+if(age == 30){
+    alert('Avtomobil sürmək üçün kifayət qədər yaşlısınız.')
+}else if(age < 18){
+    alert('Sizə maşın sürmək üçün 3 il vaxt qalır.')
+}else if(age >=18){
+    alert('Siz maşın sürə bilərsiniz.')
+}
+
